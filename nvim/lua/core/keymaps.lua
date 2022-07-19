360b5dd3352f702a067e32dddd5d3b8beb397405
shellRaining <shellRaining@gmail.com>
2022-07-19 23:07:04 +0800

    add hlslen

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -652,23 +652,90 @@ api.map.bulk_register({
     },
     {
         mode = { "n", "v" },
-        lhs = "<leader>hw",
+        lhs = "<leader><leader>w",
         rhs = "<cmd>HopWord<cr>",
         options = { silent = true },
         description = "Jump to word head",
     },
     {
         mode = { "n", "v" },
-        lhs = "<leader>hl",
+        lhs = "<leader><leader>l",
         rhs = "<cmd>HopLine<cr>",
         options = { silent = true },
         description = "Jump to line",
     },
     {
         mode = { "n", "v" },
-        lhs = "<leader>hc",
+        lhs = "<leader><leader>c",
         rhs = "<cmd>HopChar1<cr>",
         options = { silent = true },
         description = "Jump to search char on buffer",
+    },
+})
+
+--------------------------------------------------------------------------------
+-- hlslens 相关设置
+--------------------------------------------------------------------------------
+api.map.bulk_register({
+    {
+        mode = { "n" },
+        lhs = "n",
+        rhs = function()
+            pcall(vim.cmd, "normal! " .. vim.v.count1 .. "n")
+            require("hlslens").start()
+        end,
+        options = { silent = true },
+        description = "Skip to next search result",
+    },
+    {
+        mode = { "n" },
+        lhs = "N",
+        rhs = function()
+            pcall(vim.cmd, "normal! " .. vim.v.count1 .. "N")
+            require("hlslens").start()
+        end,
+        options = { silent = true },
+        description = "Jump to previous search result",
+    },
+    {
+        mode = { "n" },
+        lhs = "*",
+        rhs = function()
+            pcall(vim.cmd, "normal! " .. vim.v.count1 .. "*")
+            require("hlslens").start()
+        end,
+        options = { silent = true },
+        description = "Jump to the next word at the current cursor",
+    },
+
+    {
+        mode = { "n" },
+        lhs = "#",
+        rhs = function()
+            pcall(vim.cmd, "normal! " .. vim.v.count1 .. "#")
+            require("hlslens").start()
+        end,
+        options = { silent = true },
+        description = "Jump to the prev word at the current cursor",
+    },
+    {
+        mode = { "n" },
+        lhs = "g*",
+        rhs = function()
+            pcall(vim.cmd, "normal! " .. vim.v.count1 .. "g*")
+            require("hlslens").start()
+        end,
+        options = { silent = true },
+        description = "Jump to the next word at the current cursor (forbidden range)",
+    },
+    {
+        mode = { "n" },
+        lhs = "g#",
+        rhs = function()
+            pcall(vim.cmd, "normal! " .. vim.v.count1 .. "g#")
+            require("hlslens").start()
+        end,
+        options = { silent = true },
+        description = "Jump to the prev word at the current cursor (forbidden range)",
     },
 })
