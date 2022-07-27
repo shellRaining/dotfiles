118f73035d464606f3f4b4a4b37b9b67f839c36e
shellRaining <shellRaining@gmail.com>
2022-07-27 09:42:53 +0800

    delete main branch useless file

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -372,6 +372,24 @@ if not vim.g.packer_custom_loader_enabled then
   vim.g.packer_custom_loader_enabled = true
 end
 
+-- Setup for: nvim-colorizer.lua
+time([[Setup for nvim-colorizer.lua]], true)
+require('conf/plugins/nvim-colorizer').before()
+time([[Setup for nvim-colorizer.lua]], false)
+time([[packadd for nvim-colorizer.lua]], true)
+vim.cmd [[packadd nvim-colorizer.lua]]
+time([[packadd for nvim-colorizer.lua]], false)
+-- Setup for: nvim-hlslens
+time([[Setup for nvim-hlslens]], true)
+require('conf/plugins/nvim-hlslens').before()
+time([[Setup for nvim-hlslens]], false)
+-- Setup for: project.nvim
+time([[Setup for project.nvim]], true)
+require('conf/plugins/project').before()
+time([[Setup for project.nvim]], false)
+time([[packadd for project.nvim]], true)
+vim.cmd [[packadd project.nvim]]
+time([[packadd for project.nvim]], false)
 -- Setup for: spellsitter.nvim
 time([[Setup for spellsitter.nvim]], true)
 require('conf/plugins/spellsitter').before()
@@ -379,10 +397,6 @@ time([[Setup for spellsitter.nvim]], false)
 time([[packadd for spellsitter.nvim]], true)
 vim.cmd [[packadd spellsitter.nvim]]
 time([[packadd for spellsitter.nvim]], false)
--- Setup for: vim-carbon-now-sh
-time([[Setup for vim-carbon-now-sh]], true)
-require('conf/plugins/vim-carbon-now-sh').before()
-time([[Setup for vim-carbon-now-sh]], false)
 -- Setup for: nvim-cmp
 time([[Setup for nvim-cmp]], true)
 require('conf/plugins/nvim-cmp').before()
@@ -390,73 +404,14 @@ time([[Setup for nvim-cmp]], false)
 time([[packadd for nvim-cmp]], true)
 vim.cmd [[packadd nvim-cmp]]
 time([[packadd for nvim-cmp]], false)
--- Setup for: telescope.nvim
-time([[Setup for telescope.nvim]], true)
-require('conf/plugins/telescope').before()
-time([[Setup for telescope.nvim]], false)
-time([[packadd for telescope.nvim]], true)
-vim.cmd [[packadd telescope.nvim]]
-time([[packadd for telescope.nvim]], false)
--- Setup for: nvim-treesitter
-time([[Setup for nvim-treesitter]], true)
-require('conf/plugins/nvim-treesitter').before()
-time([[Setup for nvim-treesitter]], false)
--- Setup for: nvim-tree.lua
-time([[Setup for nvim-tree.lua]], true)
-require('conf/plugins/nvim-tree').before()
-time([[Setup for nvim-tree.lua]], false)
--- Setup for: gitsigns.nvim
-time([[Setup for gitsigns.nvim]], true)
-require('conf/plugins/gitsigns').before()
-time([[Setup for gitsigns.nvim]], false)
--- Setup for: catppuccin
-time([[Setup for catppuccin]], true)
-require('conf/plugins/catppuccin').before()
-time([[Setup for catppuccin]], false)
-time([[packadd for catppuccin]], true)
-vim.cmd [[packadd catppuccin]]
-time([[packadd for catppuccin]], false)
--- Setup for: hop.nvim
-time([[Setup for hop.nvim]], true)
-require('conf/plugins/hop').before()
-time([[Setup for hop.nvim]], false)
--- Setup for: vim-illuminate
-time([[Setup for vim-illuminate]], true)
-require('conf/plugins/vim-illuminate').before()
-time([[Setup for vim-illuminate]], false)
--- Setup for: indent-blankline.nvim
-time([[Setup for indent-blankline.nvim]], true)
-require('conf/plugins/indent-blankline').before()
-time([[Setup for indent-blankline.nvim]], false)
-time([[packadd for indent-blankline.nvim]], true)
-vim.cmd [[packadd indent-blankline.nvim]]
-time([[packadd for indent-blankline.nvim]], false)
--- Setup for: lualine.nvim
-time([[Setup for lualine.nvim]], true)
-require('conf/plugins/lualine').before()
-time([[Setup for lualine.nvim]], false)
-time([[packadd for lualine.nvim]], true)
-vim.cmd [[packadd lualine.nvim]]
-time([[packadd for lualine.nvim]], false)
+-- Setup for: vim-carbon-now-sh
+time([[Setup for vim-carbon-now-sh]], true)
+require('conf/plugins/vim-carbon-now-sh').before()
+time([[Setup for vim-carbon-now-sh]], false)
 -- Setup for: undotree
 time([[Setup for undotree]], true)
 require('conf/plugins/undotree').before()
 time([[Setup for undotree]], false)
--- Setup for: nvim-autopairs
-time([[Setup for nvim-autopairs]], true)
-require('conf/plugins/nvim-autopairs').before()
-time([[Setup for nvim-autopairs]], false)
--- Setup for: vim-vsnip
-time([[Setup for vim-vsnip]], true)
-require('conf/plugins/vim-vsnip').before()
-time([[Setup for vim-vsnip]], false)
-time([[packadd for vim-vsnip]], true)
-vim.cmd [[packadd vim-vsnip]]
-time([[packadd for vim-vsnip]], false)
--- Setup for: Comment.nvim
-time([[Setup for Comment.nvim]], true)
-require('conf/plugins/comment').before()
-time([[Setup for Comment.nvim]], false)
 -- Setup for: todo-comments.nvim
 time([[Setup for todo-comments.nvim]], true)
 require('conf/plugins/todo-comments').before()
@@ -468,31 +423,36 @@ time([[packadd for todo-comments.nvim]], false)
 time([[Setup for translate.nvim]], true)
 require('conf/plugins/translate').before()
 time([[Setup for translate.nvim]], false)
--- Setup for: nvim-notify
-time([[Setup for nvim-notify]], true)
-require('conf/plugins/nvim-notify').before()
-time([[Setup for nvim-notify]], false)
-time([[packadd for nvim-notify]], true)
-vim.cmd [[packadd nvim-notify]]
-time([[packadd for nvim-notify]], false)
--- Setup for: nvim-hlslens
-time([[Setup for nvim-hlslens]], true)
-require('conf/plugins/nvim-hlslens').before()
-time([[Setup for nvim-hlslens]], false)
--- Setup for: nvim-colorizer.lua
-time([[Setup for nvim-colorizer.lua]], true)
-require('conf/plugins/nvim-colorizer').before()
-time([[Setup for nvim-colorizer.lua]], false)
-time([[packadd for nvim-colorizer.lua]], true)
-vim.cmd [[packadd nvim-colorizer.lua]]
-time([[packadd for nvim-colorizer.lua]], false)
--- Setup for: project.nvim
-time([[Setup for project.nvim]], true)
-require('conf/plugins/project').before()
-time([[Setup for project.nvim]], false)
-time([[packadd for project.nvim]], true)
-vim.cmd [[packadd project.nvim]]
-time([[packadd for project.nvim]], false)
+-- Setup for: gitsigns.nvim
+time([[Setup for gitsigns.nvim]], true)
+require('conf/plugins/gitsigns').before()
+time([[Setup for gitsigns.nvim]], false)
+-- Setup for: Comment.nvim
+time([[Setup for Comment.nvim]], true)
+require('conf/plugins/comment').before()
+time([[Setup for Comment.nvim]], false)
+-- Setup for: hop.nvim
+time([[Setup for hop.nvim]], true)
+require('conf/plugins/hop').before()
+time([[Setup for hop.nvim]], false)
+-- Setup for: catppuccin
+time([[Setup for catppuccin]], true)
+require('conf/plugins/catppuccin').before()
+time([[Setup for catppuccin]], false)
+time([[packadd for catppuccin]], true)
+vim.cmd [[packadd catppuccin]]
+time([[packadd for catppuccin]], false)
+-- Setup for: vim-illuminate
+time([[Setup for vim-illuminate]], true)
+require('conf/plugins/vim-illuminate').before()
+time([[Setup for vim-illuminate]], false)
+-- Setup for: indent-blankline.nvim
+time([[Setup for indent-blankline.nvim]], true)
+require('conf/plugins/indent-blankline').before()
+time([[Setup for indent-blankline.nvim]], false)
+time([[packadd for indent-blankline.nvim]], true)
+vim.cmd [[packadd indent-blankline.nvim]]
+time([[packadd for indent-blankline.nvim]], false)
 -- Setup for: bufferline.nvim
 time([[Setup for bufferline.nvim]], true)
 require('conf/plugins/bufferline').before()
@@ -500,6 +460,74 @@ time([[Setup for bufferline.nvim]], false)
 time([[packadd for bufferline.nvim]], true)
 vim.cmd [[packadd bufferline.nvim]]
 time([[packadd for bufferline.nvim]], false)
+-- Setup for: vim-vsnip
+time([[Setup for vim-vsnip]], true)
+require('conf/plugins/vim-vsnip').before()
+time([[Setup for vim-vsnip]], false)
+time([[packadd for vim-vsnip]], true)
+vim.cmd [[packadd vim-vsnip]]
+time([[packadd for vim-vsnip]], false)
+-- Setup for: lualine.nvim
+time([[Setup for lualine.nvim]], true)
+require('conf/plugins/lualine').before()
+time([[Setup for lualine.nvim]], false)
+time([[packadd for lualine.nvim]], true)
+vim.cmd [[packadd lualine.nvim]]
+time([[packadd for lualine.nvim]], false)
+-- Setup for: nvim-treesitter
+time([[Setup for nvim-treesitter]], true)
+require('conf/plugins/nvim-treesitter').before()
+time([[Setup for nvim-treesitter]], false)
+-- Setup for: nvim-autopairs
+time([[Setup for nvim-autopairs]], true)
+require('conf/plugins/nvim-autopairs').before()
+time([[Setup for nvim-autopairs]], false)
+-- Setup for: nvim-notify
+time([[Setup for nvim-notify]], true)
+require('conf/plugins/nvim-notify').before()
+time([[Setup for nvim-notify]], false)
+time([[packadd for nvim-notify]], true)
+vim.cmd [[packadd nvim-notify]]
+time([[packadd for nvim-notify]], false)
+-- Setup for: nvim-tree.lua
+time([[Setup for nvim-tree.lua]], true)
+require('conf/plugins/nvim-tree').before()
+time([[Setup for nvim-tree.lua]], false)
+-- Setup for: telescope.nvim
+time([[Setup for telescope.nvim]], true)
+require('conf/plugins/telescope').before()
+time([[Setup for telescope.nvim]], false)
+time([[packadd for telescope.nvim]], true)
+vim.cmd [[packadd telescope.nvim]]
+time([[packadd for telescope.nvim]], false)
+-- Config for: nvim-notify
+time([[Config for nvim-notify]], true)
+                         require('utils.api').safe_load(require('conf/plugins/nvim-notify'))
+                         require('conf/plugins/nvim-notify').load()
+                         require('conf/plugins/nvim-notify').after()
+                         
+time([[Config for nvim-notify]], false)
+-- Config for: nvim-colorizer.lua
+time([[Config for nvim-colorizer.lua]], true)
+                         require('utils.api').safe_load(require('conf/plugins/nvim-colorizer'))
+                         require('conf/plugins/nvim-colorizer').load()
+                         require('conf/plugins/nvim-colorizer').after()
+                         
+time([[Config for nvim-colorizer.lua]], false)
+-- Config for: project.nvim
+time([[Config for project.nvim]], true)
+                         require('utils.api').safe_load(require('conf/plugins/project'))
+                         require('conf/plugins/project').load()
+                         require('conf/plugins/project').after()
+                         
+time([[Config for project.nvim]], false)
+-- Config for: catppuccin
+time([[Config for catppuccin]], true)
+                         require('utils.api').safe_load(require('conf/plugins/catppuccin'))
+                         require('conf/plugins/catppuccin').load()
+                         require('conf/plugins/catppuccin').after()
+                         
+time([[Config for catppuccin]], false)
 -- Config for: spellsitter.nvim
 time([[Config for spellsitter.nvim]], true)
                          require('utils.api').safe_load(require('conf/plugins/spellsitter'))
@@ -514,6 +542,13 @@ time([[Config for indent-blankline.nvim]], true)
                          require('conf/plugins/indent-blankline').after()
                          
 time([[Config for indent-blankline.nvim]], false)
+-- Config for: nvim-cmp
+time([[Config for nvim-cmp]], true)
+                         require('utils.api').safe_load(require('conf/plugins/nvim-cmp'))
+                         require('conf/plugins/nvim-cmp').load()
+                         require('conf/plugins/nvim-cmp').after()
+                         
+time([[Config for nvim-cmp]], false)
 -- Config for: telescope.nvim
 time([[Config for telescope.nvim]], true)
                          require('utils.api').safe_load(require('conf/plugins/telescope'))
@@ -535,27 +570,6 @@ time([[Config for todo-comments.nvim]], true)
                          require('conf/plugins/todo-comments').after()
                          
 time([[Config for todo-comments.nvim]], false)
--- Config for: nvim-cmp
-time([[Config for nvim-cmp]], true)
-                         require('utils.api').safe_load(require('conf/plugins/nvim-cmp'))
-                         require('conf/plugins/nvim-cmp').load()
-                         require('conf/plugins/nvim-cmp').after()
-                         
-time([[Config for nvim-cmp]], false)
--- Config for: nvim-notify
-time([[Config for nvim-notify]], true)
-                         require('utils.api').safe_load(require('conf/plugins/nvim-notify'))
-                         require('conf/plugins/nvim-notify').load()
-                         require('conf/plugins/nvim-notify').after()
-                         
-time([[Config for nvim-notify]], false)
--- Config for: catppuccin
-time([[Config for catppuccin]], true)
-                         require('utils.api').safe_load(require('conf/plugins/catppuccin'))
-                         require('conf/plugins/catppuccin').load()
-                         require('conf/plugins/catppuccin').after()
-                         
-time([[Config for catppuccin]], false)
 -- Config for: vim-vsnip
 time([[Config for vim-vsnip]], true)
                          require('utils.api').safe_load(require('conf/plugins/vim-vsnip'))
@@ -563,20 +577,6 @@ time([[Config for vim-vsnip]], true)
                          require('conf/plugins/vim-vsnip').after()
                          
 time([[Config for vim-vsnip]], false)
--- Config for: nvim-colorizer.lua
-time([[Config for nvim-colorizer.lua]], true)
-                         require('utils.api').safe_load(require('conf/plugins/nvim-colorizer'))
-                         require('conf/plugins/nvim-colorizer').load()
-                         require('conf/plugins/nvim-colorizer').after()
-                         
-time([[Config for nvim-colorizer.lua]], false)
--- Config for: project.nvim
-time([[Config for project.nvim]], true)
-                         require('utils.api').safe_load(require('conf/plugins/project'))
-                         require('conf/plugins/project').load()
-                         require('conf/plugins/project').after()
-                         
-time([[Config for project.nvim]], false)
 -- Config for: bufferline.nvim
 time([[Config for bufferline.nvim]], true)
                          require('utils.api').safe_load(require('conf/plugins/bufferline'))
@@ -591,30 +591,30 @@ time([[Sequenced loading]], false)
 
 -- Command lazy-loads
 time([[Defining lazy-load commands]], true)
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file HopChar1CurrentLine lua require("packer.load")({'hop.nvim'}, { cmd = "HopChar1CurrentLine", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file Translate lua require("packer.load")({'translate.nvim'}, { cmd = "Translate", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file NvimTreeFindFile lua require("packer.load")({'nvim-tree.lua'}, { cmd = "NvimTreeFindFile", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file CarbonNowSh lua require("packer.load")({'vim-carbon-now-sh'}, { cmd = "CarbonNowSh", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file NvimTreeToggle lua require("packer.load")({'nvim-tree.lua'}, { cmd = "NvimTreeToggle", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file HopWord lua require("packer.load")({'hop.nvim'}, { cmd = "HopWord", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file HopLine lua require("packer.load")({'hop.nvim'}, { cmd = "HopLine", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file HopChar1 lua require("packer.load")({'hop.nvim'}, { cmd = "HopChar1", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file HopChar1CurrentLine lua require("packer.load")({'hop.nvim'}, { cmd = "HopChar1CurrentLine", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file CarbonNowSh lua require("packer.load")({'vim-carbon-now-sh'}, { cmd = "CarbonNowSh", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file NvimTreeToggle lua require("packer.load")({'nvim-tree.lua'}, { cmd = "NvimTreeToggle", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 time([[Defining lazy-load commands]], false)
 
 -- Keymap lazy-loads
 time([[Defining lazy-load keymaps]], true)
-vim.cmd [[noremap <silent> gc <cmd>lua require("packer.load")({'Comment.nvim'}, { keys = "gc", prefix = "" }, _G.packer_plugins)<cr>]]
 vim.cmd [[noremap <silent> gb <cmd>lua require("packer.load")({'Comment.nvim'}, { keys = "gb", prefix = "" }, _G.packer_plugins)<cr>]]
+vim.cmd [[noremap <silent> gc <cmd>lua require("packer.load")({'Comment.nvim'}, { keys = "gc", prefix = "" }, _G.packer_plugins)<cr>]]
 time([[Defining lazy-load keymaps]], false)
 
 vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Event lazy-loads
 time([[Defining lazy-load event autocommands]], true)
-vim.cmd [[au BufRead * ++once lua require("packer.load")({'undotree', 'vim-illuminate', 'gitsigns.nvim'}, { event = "BufRead *" }, _G.packer_plugins)]]
-vim.cmd [[au BufNewFile * ++once lua require("packer.load")({'undotree', 'vim-illuminate', 'gitsigns.nvim'}, { event = "BufNewFile *" }, _G.packer_plugins)]]
-vim.cmd [[au CmdlineEnter * ++once lua require("packer.load")({'friendly-snippets'}, { event = "CmdlineEnter *" }, _G.packer_plugins)]]
+vim.cmd [[au BufRead * ++once lua require("packer.load")({'gitsigns.nvim', 'vim-illuminate', 'undotree'}, { event = "BufRead *" }, _G.packer_plugins)]]
+vim.cmd [[au BufNewFile * ++once lua require("packer.load")({'gitsigns.nvim', 'vim-illuminate', 'undotree'}, { event = "BufNewFile *" }, _G.packer_plugins)]]
 vim.cmd [[au InsertEnter * ++once lua require("packer.load")({'friendly-snippets', 'nvim-autopairs'}, { event = "InsertEnter *" }, _G.packer_plugins)]]
+vim.cmd [[au CmdlineEnter * ++once lua require("packer.load")({'friendly-snippets'}, { event = "CmdlineEnter *" }, _G.packer_plugins)]]
 time([[Defining lazy-load event autocommands]], false)
 vim.cmd("augroup END")
 if should_profile then save_profiles() end
