88c6807e1c0ea230de693660004184c8282d9812
sbccznfy <1579786177@qq.com>
2022-08-01 14:44:16 +0800

    dont update zshrc, and update gitconfig, set treesitter config for all language

--- nvim/lua/user/conf/treesitter.lua
+++ nvim/lua/user/conf/treesitter.lua
@@ -5,9 +5,9 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = { "cpp", "c", "python", "markdown", "json", "yaml", "lua" },
+  ensure_installed = "all",
+  ignore_install = { "phpdoc", "jsonc", "fusion" }, -- List of parsers to ignore installing
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
   highlight = {
     enable = true,
     disable = { "" },
